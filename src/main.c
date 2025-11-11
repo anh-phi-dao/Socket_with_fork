64e53dea5b74c87609cfb9069b9f5a89c44708a2
Phi <daoanhphi382@gmail.com>
2025-11-11 09:10:15 +0700

    delete thread

--- src/main.c
+++ src/main.c
@@ -1,4 +1,4 @@
-#include "manage_server_with_thread.h"
+#include "multilple_client_server.h"
 #include "socket.h"
 #include <sys/poll.h>
 #include <pthread.h>
@@ -53,78 +53,6 @@ int main()
             {
                 printf("Connection to this client has failed\n");
                 return -1;
-            }
-            for (int i = 0; i < MAXIMUM_CLIENT; i++)
-            {
-                if (client_fd[i] == 0)
-                {
-                    client_fd[i] = new_fd;
-                    read_fdps[i].fd = new_fd;
-                    if (pthread_create(&manage_client_threads[i], NULL, handle_message_thread, read_fdps + i) > 0)
-                    {
-                        perror("pthread_create");
-                        printf("Error on pthread\n");
-                        close(server_fd);
-                        mq_unlink(MANAGE_SERVER_QUEUE_NAME);
-                        return -1;
-                    }
-                    num_of_thread++;
-                    break;
-                }
-            }
-        }
-        /*in order to avoid mutex blocking, the number of thread must be >0*/
-        /*get the global string status message after thread has unlocked the mutex*/
-        /*compare the message for each case, FIND_FILE, CLOSE_FILE, DISCONNECT_CLIENT*/
-        if (num_of_thread > 0)
-        {
-            state = pthread_mutex_lock(&server_mtx);
-            if (state != 0)
-            {
-                printf("Error locking mutex\n");
-            }
-
-            /*when receiving CLOSE_FILE_MESSAGE, close all clients, detach all running thread*/
-            if (strcmp(status_message, CLOSE_FILE_MESSAGE) == 0)
-            {
-                printf("Closing all file\n");
-                for (int i = 0; i < MAXIMUM_CLIENT; i++)
-                {
-                    if (client_fd[i] != 0)
-                    {
-                        writen(client_fd[i], "Closing connect from server\n", 29);
-                        pthread_detach(manage_client_threads[i]);
-                        close(client_fd[i]);
-                        client_fd[i] = 0;
-                    }
-                }
-                num_of_thread = 0;
-                break;
-            }
-            /*when receiving CLIENT_DISCONNECTED_MESSAGE, close the disconnected clients, detach thread of
-            disconnected client*/
-            else if (strcmp(buff, CLIENT_DISCONNECTED_MESSAGE) == 0)
-            {
-                printf("Disconnect now\n");
-                for (int i = 0; i < MAXIMUM_CLIENT; i++)
-                {
-                    if (client_fd[i] != read_fdps[i].fd)
-                    {
-                        pthread_detach(manage_client_threads[i]);
-                        close(client_fd[i]);
-                        client_fd[i] = 0;
-                        if (num_of_thread > 0)
-                        {
-                            num_of_thread--;
-                        }
-                        break;
-                    }
-                }
-            }
-            state = pthread_mutex_unlock(&server_mtx);
-            if (state != 0)
-            {
-                printf("Error locking mutex\n");
             }
         }
     }
