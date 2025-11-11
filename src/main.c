ba2939cc62451f0a62884a51022dfbf8eaf8d4f2
Phi <daoanhphi382@gmail.com>
2025-11-11 14:53:50 +0700

    Successful create fork

--- src/main.c
+++ src/main.c
@@ -4,6 +4,7 @@
 #include <string.h>
 #include <fcntl.h>
 #include <sys/wait.h>
+#include <sys/poll.h>
 #include <pthread.h>
 #include <sys/shm.h>
 #include <sys/stat.h>
@@ -11,23 +12,24 @@
 #include <unistd.h>
 
 #define OBJECT_NAME "/shared_zone"
-#define SIZE 1024
+#define SIZE 200
 
 char buff[1024];
 char message[1024];
 int ret, state;
-
-int num_of_thread = 0;
+struct pollfd connect_fd;
+struct pollfd read_fdp;
 pid_t client_process_id, childPID;
 int val_read;
 int shared_mem_fd;
-
 int message_handling = 0;
-#include <sys/wait.h>
+char *ptr;
+
 int main()
 {
 
     /*create an TCP server using IPv4 address and defined maximum client*/
+    shm_unlink(OBJECT_NAME);
     int i = 0;
     do
     {
@@ -50,59 +52,103 @@ int main()
         exit(EXIT_FAILURE);
     }
 
+    ptr = mmap(NULL, SIZE, PROT_READ | PROT_WRITE, MAP_SHARED, shared_mem_fd, 0);
+
     if (create_TCP_IPv4_server(&server, PORT, MAXIMUM_CLIENT, &server_fd, &len) == ERROR)
     {
         return -1;
     }
 
+    connect_fd.fd = server_fd;
+    connect_fd.events = POLLIN;
     printf("Ready to connect \n");
+    int process_num = 0;
 
     while (1)
     {
-
-        if (accept_client_connection(&server, &server_fd, &client_fd, &len) == ERROR)
+        ret = poll(&connect_fd, 1, 100);
+        if (ret > 0)
         {
-            printf("Connection to this client has failed\n");
-            return -1;
-        }
-        client_process_id = fork();
-
-        if (client_process_id == 0)
-        {
-            while (childPID == 0)
+            if (accept_client_connection(&server, &server_fd, &client_fd, &len) == ERROR)
             {
-
-                val_read = read(client_fd, buff, 1024);
-                if (val_read > 0)
-                {
-                    message_handling = handling_message_for_multiple_clients(&client_fd, buff);
-                    if (message_handling == FIND_FILE)
-                    {
-                        find_file_following_client_request(message, buff, &client_fd);
-                    }
-                    else if (message_handling == CLOSE_MESSAGE)
-                    {
-                        close(client_fd);
-                        exit(EXIT_SUCCESS);
-                    }
-                    else if (message_handling == DISCONNECTED)
-                    {
-                        exit(EXIT_SUCCESS);
-                    }
-                }
+                printf("Connection to this client has failed\n");
+                return -1;
+            }
+            process_num++;
+            client_process_id = fork();
+            if (client_process_id == 0)
+            {
+                break;
+            }
+            else if (client_process_id < 0)
+            {
+                close(server_fd);
+                exit(EXIT_FAILURE);
             }
         }
+
+        if (strcmp(ptr, "Close") == 0)
+        {
+            break;
+        }
     }
 
-    childPID = wait(NULL);
-    if (childPID != -1)
+    if (client_process_id == 0)
     {
-        printf("Close client in PID %d\n", childPID);
+        printf("Client is using process with %d \n", getpid());
+        read_fdp.fd = client_fd;
+        read_fdp.events = POLLIN;
     }
-    else
+    while (client_process_id == 0)
     {
-        printf("No child need to be closed\n");
+        if (strcmp(ptr, "Close") == 0)
+        {
+            close(client_fd);
+            exit(EXIT_SUCCESS);
+        }
+
+        ret = poll(&read_fdp, 1, 10);
+        if (ret == 0)
+        {
+            continue;
+        }
+        else if (ret < 0)
+        {
+            close(client_fd);
+            exit(EXIT_FAILURE);
+        }
+        message_handling = handling_message_for_multiple_clients(&client_fd, message);
+        if (message_handling == FIND_FILE)
+        {
+            printf("Finding file\n");
+            find_file_following_client_request(message, buff, &client_fd);
+        }
+        else if (message_handling == CLOSE_MESSAGE)
+        {
+            close(client_fd);
+            printf("Close server\n");
+            sprintf(ptr, "Close");
+            exit(EXIT_SUCCESS);
+        }
+        else if (message_handling == DISCONNECTED)
+        {
+            exit(EXIT_SUCCESS);
+        }
     }
+
+    for (size_t i = 0; i < process_num; i++)
+    {
+        childPID = wait(NULL);
+        if (childPID != -1)
+        {
+            printf("Close client in PID %d\n", childPID);
+        }
+        else
+        {
+            printf("No child need to be closed\n");
+        }
+    }
+
     shm_unlink(OBJECT_NAME);
     return 0;
 }
